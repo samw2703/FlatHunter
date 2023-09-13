33ef91cfd88cedbfb8d9034dfe61c6bca8fee0a3
samw2703 <samw2703@gmail.com>
2023-09-13 21:52:49 +0100

    made a start

--- FlatHunter.Console/Program.cs
+++ FlatHunter.Console/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BaseCLI;
+using FlatHunter.Console;
+
 Console.WriteLine("Hello, World!");
+
+CLI.Execute(args, new[] { typeof(TestArgs).Assembly }, services => { });
---
+++ FlatHunter.Console/TestCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseCLI;
+using FlatHunter.Crawler.Selenium;
+using OpenQA.Selenium;
+
+namespace FlatHunter.Console;
+
+internal class TestCommand : ICommand<TestArgs>
+{
+    public void Execute(TestArgs args)
+    {
+        WebBrowser.Launch().Test();
+    }
+
+    public string Name { get; } = "Test";
+    public string Description { get; } = "Test";
+
+    public ArgInfoCollection<TestArgs> ArgInfoCollection { get; } = new ArgInfoBuilder<TestArgs>()
+        .Build();
+}
+
+internal class TestArgs {}
